407a4618f27a75c22bc37f8c958736772f2435f0
yyrrcc <music_v@naver.com>
2025-10-30 17:50:55 +0900

    토큰

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import api from "./axiosConfig";
 
@@ -7,12 +7,14 @@ function App() {
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
 
+  // 토큰값 (localStorage : 웹브라우저에서 기본적으로 가지고 있는 저장소)
+  const [token, setToken] = useState("" || localStorage.getItem("token"));
+
   // 회원가입
   const handleSignup = async (e) => {
     e.preventDefault();
     try {
-      // URLSearchParams 이용해서 파라미터 값 넣어주기
-      await api.post("/api/auth/signup", new URLSearchParams({ username, password }));
+      await api.post("/api/auth/signup", { username, password }); // JSON 타입으로 넣어주기
       setMessage(username + "님 회원가입 성공");
     } catch (error) {
       console.error(error);
@@ -24,7 +26,9 @@ function App() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      await api.post("/api/auth/login", new URLSearchParams({ username, password }));
+      const res = await api.post("/api/auth/login", { username, password });
+      setToken(res.data.token); // 토큰값 저장
+      localStorage.setItem("token", res.data.token);
       setMessage(username + "님 로그인 성공");
     } catch (error) {
       console.error(error);
@@ -32,23 +36,27 @@ function App() {
     }
   };
 
-  // 로그아웃
-  const handleLogout = async () => {
-    await api.post("/api/auth/logout");
+  // 로그아웃 (= 토큰 삭제 및 토큰값 초기화)
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setToken("");
     setMessage(username + "로그아웃 성공");
   };
 
   // 로그인한 사용자 확인
   const handleLoginCheck = async () => {
     try {
-      const res = await api.get("/api/auth/me");
+      // 로그인하지 않은 경우(= 토큰이 없는 경우)
+      if (!token) {
+        alert("로그인이 필요합니다.");
+        return;
+      }
+      // Authorization 헤더를 넣어서 인증 정보를 보내줘야 함!!
+      const res = await api.get("/api/auth/me", { headers: { Authorization: `Bearer ${token}` } });
       setMessage("현재 로그인한 사용자 : " + res.data.username);
     } catch (error) {
-      if (error.response && error.response.status === 401) {
-        setMessage("로그인이 필요합니다.");
-      } else {
-        setMessage("에러 발생: " + error.message);
-      }
+      console.error(error);
+      alert("에러났어요");
     }
   };
 
--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8888", // EC2의 ip주소:포트 넣어줘야 함
-  withCredentials: true,
+  // withCredentials: true,
 });
 
 export default api;
