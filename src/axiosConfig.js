ad51ad8e7ef5278dff56abc027fee910a0140932
yyrrcc <music_v@naver.com>
2025-10-31 09:11:54 +0900

    aws jwt build

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8888", // EC2의 ip주소:포트 넣어줘야 함
-  // withCredentials: true,
+  // baseURL: "http://localhost:8888",
+  baseURL: "http://15.165.98.233", // EC2의 ip주소 넣어줘야 함
 });
 
 export default api;
